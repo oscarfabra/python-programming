10362c1daec954e4bb3617141dee6cfa741537ad
Oscar Fabra <oscarfabra@gmail.com>
2020-08-19 18:43:00 -0500

    Hides keys for security reasons

--- fcc-scientific-computing-with-python/py4e/hidden.py
+++ fcc-scientific-computing-with-python/py4e/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-  return {"consumer_key": "w5Ps1VxA1JFRgpUCYH2OazuDd",
-          "consumer_secret": "NOxSAO7BGdcU1Zk2HnuMtQZoIa6Lc2Lv8QFvSYZukB7wcrqzIS",
-          "token_key": "86076548-olRWNukCgKZ5pNCOErhVzKhv6ZKYncDUbVssfSFPO",
-          "token_secret": "f3bhFKvtUm0w5pT0W3tvH8elsA55gEpylP8fLxfmFzYsA"}
+  return {"consumer_key": "xyz",
+          "consumer_secret": "xyz",
+          "token_key": "xyz",
+          "token_secret": "xyz"}
