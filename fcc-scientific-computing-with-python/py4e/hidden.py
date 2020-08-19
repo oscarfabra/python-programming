d9cd184c6aacd8706187fe444760be4a075101e9
Oscar Fabra <oscarfabra@gmail.com>
2020-08-19 18:41:04 -0500

    Adds solution to chapter 13 exercises

---
+++ fcc-scientific-computing-with-python/py4e/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+  return {"consumer_key": "w5Ps1VxA1JFRgpUCYH2OazuDd",
+          "consumer_secret": "NOxSAO7BGdcU1Zk2HnuMtQZoIa6Lc2Lv8QFvSYZukB7wcrqzIS",
+          "token_key": "86076548-olRWNukCgKZ5pNCOErhVzKhv6ZKYncDUbVssfSFPO",
+          "token_secret": "f3bhFKvtUm0w5pT0W3tvH8elsA55gEpylP8fLxfmFzYsA"}
