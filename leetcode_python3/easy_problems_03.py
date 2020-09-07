d5caeff43bc56c769c3409c2818ec0aa9173f7b1
Oscar Fabra <oscarfabra@gmail.com>
2020-09-07 02:34:18 -0500

    Adds another easy problem solution

--- leetcode_python3/easy_problems_03.py
+++ leetcode_python3/easy_problems_03.py
@@ -9,5 +9,28 @@ def diagonal_sum(mat):
     total += mat[i][-1 - i]
   return total
 
-print(diagonal_sum([[1,2,3],[4,5,6],[7,8,9]]))
-print(diagonal_sum([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]))
+# print(diagonal_sum([[1,2,3],[4,5,6],[7,8,9]]))
+# print(diagonal_sum([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]))
+
+
+# A valid parentheses string is either empty (""), "(" + A + ")", or A + B, 
+# where A and B are valid parentheses strings, and + represents string concatenation.  
+# For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+# A valid parentheses string S is primitive if it is nonempty, and there does not 
+# exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+# Given a valid parentheses string S, consider its primitive decomposition: 
+# S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+# Return S after removing the outermost parentheses of every primitive string in the 
+# primitive decomposition of S.
+def remove_outer_parentheses(S):
+  output = ""
+  ps, start = 0, 0
+  for i in range(len(S)):
+    if S[i] == "(": ps += 1
+    if S[i] == ")": ps -= 1
+    if ps == 0: output += S[start + 1:i]; start = i + 1
+  return output
+
+# print(remove_outer_parentheses("(()())(())"))
+# print(remove_outer_parentheses("(()())(())(()(()))"))
+# print(remove_outer_parentheses("()()"))
