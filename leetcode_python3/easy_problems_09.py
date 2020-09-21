2147af9c93a611238835d43027699a62f6b2b0aa
Oscar Fabra <oscarfabra@gmail.com>
2020-09-21 17:49:49 -0500

    Adds solution to 5 more easy problems

--- leetcode_python3/easy_problems_09.py
+++ leetcode_python3/easy_problems_09.py
@@ -80,3 +80,94 @@ def divisor_game(N):
 
 # print(divisor_game(2))
 # print(divisor_game(3))
+
+
+# 824. Goat Latin
+def to_goat_latin(S):
+  goat = []
+  i = 1
+  for word in S.split():
+    if word[0] in "aeiou" or word[0] in "AEIOU": word += "ma"
+    else: word = word[1:] + word[0] + "ma"
+    word += "a" * i
+    goat += [word]
+    i += 1
+  return " ".join(goat)
+
+# print(to_goat_latin("I speak Goat Latin"))
+# print(to_goat_latin("The quick brown fox jumped over the lazy dog"))
+
+
+# 1030. Matrix Cells in Distance Order
+def all_cells_dist_order(R, C, r0, c0):
+  cells = {}
+  for i in range(R):
+    for j in range(C):
+      dist = abs(r0 - i) + abs(c0 - j)
+      cells[dist] = cells.get(dist, []) + [[i, j]]
+  output = []
+  for k, v in sorted(cells.items()):
+    output += v
+  return output
+
+# print(all_cells_dist_order(1, 2, 0, 0))
+# print(all_cells_dist_order(2, 2, 0, 1))
+# print(all_cells_dist_order(2, 3, 1, 2))
+
+
+# 908. Smallest Range I
+def choose_x(num, K, mid):
+  if num <= mid:
+    if mid - num >= K: return K
+    else: return mid - num
+  else:
+    if num - mid >= K: return -K
+    else: return mid - num
+
+def smallest_range_I(A, K):
+  mid = min(A) + (max(A) - min(A)) // 2
+  B = []
+  for num in A:
+    x = choose_x(num, K, mid)
+    B += [num + x]
+  return max(B) - min(B)
+
+# print(smallest_range_I([1], 0))
+# print(smallest_range_I([0,10], 2))
+# print(smallest_range_I([1,3,6], 3))
+
+
+# 463. Island Perimeter
+def sides_surrounded_by_water(grid, R, C, row, col):
+  sides = 0
+  if row == 0 or grid[row - 1][col] == 0: sides += 1
+  if row == R - 1 or grid[row + 1][col] == 0: sides += 1
+  if col == C - 1 or grid[row][col + 1] == 0: sides += 1
+  if col == 0 or grid[row][col - 1] == 0: sides += 1
+  return sides
+
+def island_perimeter(grid):
+  p = 0
+  R = len(grid)
+  C = len(grid[0])
+  for i in range(R):
+    for j in range(C):
+      if grid[i][j] == 1: p += sides_surrounded_by_water(grid, R, C, i, j)
+  return p
+
+# print(island_perimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]))
+# print(island_perimeter([[1]]))
+# print(island_perimeter([[1,0]]))
+
+
+# 136. Single Number
+def single_number(nums):
+  count = {}
+  for num in nums:
+    count[num] = count.get(num, 0) + 1
+  for num, times in count.items():
+    if times == 1: return num
+  return None
+
+# print(single_number([2,2,1]))
+# print(single_number([4,1,2,1,2]))
