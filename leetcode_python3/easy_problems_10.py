5bb5b8bdde041ded175d73aa53c03151efbd16bd
Oscar Fabra <oscarfabra@gmail.com>
2020-09-23 23:59:44 -0500

    Adds 5 more solutions to easy problems

--- leetcode_python3/easy_problems_10.py
+++ leetcode_python3/easy_problems_10.py
@@ -81,3 +81,85 @@ def number_of_lines(widths, S):
 
 # print(number_of_lines([10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "abcdefghijklmnopqrstuvwxyz"))
 # print(number_of_lines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], "bbbcccdddaaa"))
+
+
+# 1413. Minimum Value to Get Positive Step by Step Sum
+def min_start_value(nums):
+  start_value = 1
+  while (True):
+    i = 0
+    total = start_value
+    while i < len(nums):
+      total += nums[i]
+      if total < 1: break
+      i += 1
+    if i == len(nums): return start_value
+    start_value += 1
+
+# print(min_start_value([-3,2,-3,4,2]))
+# print(min_start_value([1,2]))
+# print(min_start_value([1,-2,-3]))
+
+
+# 500. Keyboard Row
+def word_in_row(word, row):
+  for c in word:
+    if c.lower() not in row: return False
+  return True
+
+def find_words(words):
+  rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+  result = []
+  for word in words:
+    for row in rows:
+      if word_in_row(word, row): result += [word]; break
+  return result
+
+# print(find_words(["Hello", "Alaska", "Dad", "Peace"]))
+
+
+# 476. Number Complement
+def flip_bits(binary):
+  result = ""
+  for bit in binary:
+    result += "0" if bit == "1" else "1"
+  return result
+
+def find_complement(num):
+  num_bin = "{0:016b}".format(num)
+  num_bin = num_bin.lstrip("0")
+  comp_bin = flip_bits(num_bin)
+  return int(comp_bin, 2)
+
+# print(find_complement(5))
+# print(find_complement(1))
+# print(find_complement(123))
+
+
+# 1217. Minimum Cost to Move Chips to The Same Position
+def min_cost_to_move_chips(position):
+  evens = 0
+  for pos in position:
+    if pos % 2 == 0: evens += 1
+  odds = len(position) - evens
+  if evens >= odds: return odds
+  else: return evens
+
+# print(min_cost_to_move_chips([1,2,3]))
+# print(min_cost_to_move_chips([2,2,2,3,3]))
+# print(min_cost_to_move_chips([1,1000000000]))
+
+
+# 237. Delete Node in a Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+def delete_node(node):
+  """
+  :type node: ListNode
+  :rtype: void Do not return anything, modify node in-place instead.
+  """
+  node.val = node.next.val
+  node.next = node.next.next
